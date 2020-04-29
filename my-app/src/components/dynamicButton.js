16170e23e11076ecd28c4d767bf8be4b6df562c0
Ana Luisa Lamberto <allamberto@gmail.com>
2020-04-29 06:09:38 +0000

    Signup 4 Hooked Up

--- my-app/src/components/dynamicButton.js
+++ my-app/src/components/dynamicButton.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import Textbox from './textbox';
-import ImageUpload from './image';
+import X from './../img/x.png';
+import './../css/dynamicButton.css';
 
 function Funfact(props) {
     return (
       <div>
-	<Textbox placeholder="Enter a caption" header="Fun Fact Caption" className="form-control"/>
-        <ImageUpload callback={props.callback}/>
+	<p className="section-subclass">Fun Fact Caption</p>
+	<input placeholder="Enter Fun Fact" className="form-control prompt-control" onChange={event => props.callback(event.target.value, props.num)}/>
+        <input type="file" className="file-input" onChange={event => props.fileCallback(event.target.value, props.num)}/>
       </div>
     );
+}
+
+class LoadOld extends React.Component {
+    render() {
+	var rows = [];
+	
+	for(var i of this.props.items) {
+         var id = i.id;
+	     rows.push(
+		<div className="delete-funfact">
+		  <p className="section-subclass delete-funfact-text">{i.caption}</p>
+		  <img src={X} className="delete-funfact-image" onClick={this.props.deleteCallback.bind(this, id)}/>
+		</div>
+	     );
+	}
+	return rows;	
+    }
 }
 
 class DynamicButton extends React.Component {
@@ -23,10 +41,11 @@ class DynamicButton extends React.Component {
         return(
             <div>
             <div id="dynamicInput">
-                {this.state.inputs.map(input => <Funfact callback={this.props.callback}/>)}
+		        <LoadOld items={this.props.oldFunfacts} deleteCallback={this.props.deleteCallback}/>
+                {this.state.inputs.map(input => <Funfact fileCallback={this.props.fileCallback} callback={this.props.callback} num={this.state.inputs.length}/>)}
             </div>
-            <div className="plus-container">
-               <button className="plus" onClick={ () => this.appendInput() } />
+            <div className="plus-container-dynamic">
+               <button className="plus-dynamic" onClick={ () => this.appendInput() }/>
             </div>
             </div>
         );
--- my-app/src/pages/signup_4.js
+++ my-app/src/pages/signup_4.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { NavLink, withRouter} from "react-router-dom";
 import "./../css/signup.css";
+import "./../css/signup_4.css";
 import StepProgressBar from "./../components/progressbar";
 import ImageUpload from './../components/image';
 import Textbox from './../components/textbox';
@@ -27,24 +28,25 @@ constructor(props) {
       this.state = {
 	netid: sessionStorage.getItem("netid"),
 	profile: "",
+	promptQ: "",
+	funfacts: [],
+	funfactImages: [],
+	calendar: "",
+	oldFunfacts: [],
         path: pathOp
       }
 
       this.changePage = this.changePage.bind(this);
       this.setProfile = this.setProfile.bind(this);
-    }
+      this.setPrompt = this.setPrompt.bind(this);
+      this.setFunfacts = this.setFunfacts.bind(this);
+      this.setFileFunfacts = this.setFileFunfacts.bind(this);
+      this.setCalendar = this.setCalendar.bind(this);
+      this.setDeleteFunfact = this.setDeleteFunfact.bind(this);
+}
 
-    changePage  = (event) => {
-      event.preventDefault();
-      console.log(this.state.selected);
-      const requestOptions = {
-            method: 'POST',
-	    'Content-Type': 'image/jpeg',
-            body: JSON.stringify({
-		"image" : this.state.profile
-            })
-          };
-          fetch('http://3.211.82.27:8800/students/'+this.state.netid, requestOptions)
+componentDidMount() {
+	fetch('http://3.211.82.27:8800/students/'+this.state.netid)
           .then(async response => {
                 const data = await response.json();
 
@@ -55,16 +57,188 @@ constructor(props) {
                     return Promise.reject(error);
                 }
 
-                this.props.history.push(this.state.path);
+		this.setState({promptQ: data.question}); 
             })
             .catch(error => {
-                console.error('There was an error!', error);
+                console.error('There was an error with profile pic!', error);
             });
-    } 
 
-    setProfile(target) {
-	this.setState({profile: target.dataURL});
-    } 
+	fetch('http://3.211.82.27:8800/funfacts/'+this.state.netid)
+          .then(async response => {
+                const data = await response.json();
+                
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                console.log(data);
+                this.setState({oldFunfacts: data});
+            })
+            .catch(error => {
+                console.error('There was an error with profile pic!', error);
+            });
+}
+
+    changePage  = (event) => {
+    
+      console.log(this.state.netid);
+      console.log(this.state.funfacts); 
+      console.log(this.state.funfactImages); 
+      console.log(this.state.profile); 
+      console.log(this.state.promptQ); 
+      console.log(this.state.calendar); 
+      event.preventDefault();
+    
+      if(this.state.profile != "") {
+          const requestOptionsProfilePic = {
+            method: 'POST',
+	    headers: { 'Content-Type': 'image/jpeg' },
+            body: this.state.profile
+          };
+          fetch('http://3.211.82.27:8800/students/'+this.state.netid, requestOptionsProfilePic)
+          .then(async response => {
+                const data = await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+	    })
+            .catch(error => {
+                console.error('There was an error with profile pic!', error);
+            });
+	}
+
+	for(var idx = 0; idx < this.state.funfacts.length; idx++) {
+     if(this.state.funfacts[idx] != "") {
+         const requestOptionsFunfacts = {
+            method: 'POST',
+            headers: { 'Content-Type': 'image/jpeg' },
+            body: this.state.funfactImages[idx]
+          
+          };
+          fetch('http://3.211.82.27:8800/funfacts/'+this.state.netid+"?caption="+this.state.funfacts[idx], requestOptionsFunfacts)
+          .then(async response => {
+            const data = await response.json();
+            
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || response.status;
+                return Promise.reject(error);
+            }
+              
+            })
+            .catch(error => {
+                console.error('There was an error with funfacts!', error);
+            });
+      }
+	}
+
+	const requestOptionsPrompts = {
+	    method: 'PUT',
+	    body: JSON.stringify({"question" : this.state.promptQ})
+	  };
+	  fetch('http://3.211.82.27:8800/students/'+this.state.netid, requestOptionsPrompts)
+	  .then(async response => {
+		const data = await response.json();
+
+		// check for error response
+		if (!response.ok) {
+		    // get error message from body or default to response status
+		    const error = (data && data.message) || response.status;
+		    return Promise.reject(error);
+		}
+
+	     })
+	    .catch(error => {
+		console.error('There was an error !', error);
+	    });
+
+	if(this.state.calendar != "") {
+	  const requestOptionsCalendar = {
+	    method: 'POST',
+	    body: this.state.calendar
+
+	  };
+	  fetch('http://3.211.82.27:8800/schedules/'+this.state.netid, requestOptionsCalendar)
+	  .then(async response => {
+		const data = await response.json();
+
+		// check for error response
+		if (!response.ok) {
+		    // get error message from body or default to response status
+		    const error = (data && data.message) || response.status;
+		    return Promise.reject(error);
+		}
+
+	    })
+	    .catch(error => {
+		console.error('There was an error!', error);
+	    });
+	}
+
+    this.props.history.push(this.state.path);
+  } 
+
+    setProfile = (event) => { 
+	console.log(event.target.files[0]);
+	this.setState({profile: event.target.files[0]});
+    }
+
+   setPrompt(e) {
+	this.setState({promptQ: e.target.value});
+   } 
+
+   setFunfacts(target, num) {
+   	const { funfacts } = this.state;	
+	funfacts[num - 1] = target;	
+	this.setState({ funfacts });
+   }
+
+   setFileFunfacts(target, num) {
+	const { funfactImages } = this.state;
+        funfactImages[num - 1] = target;
+        this.setState({ funfactImages });
+   }
+
+   setCalendar(e) {
+	this.setState({calendar: e.target.files[0]});
+   }
+
+   setDeleteFunfact(id) {
+	   var { oldFunfacts } =  this.state;
+        for(var i = 0; i < oldFunfacts.length; i++) {
+            if(oldFunfacts[i]['id'] == id) {
+                oldFunfacts.splice(i, 1);
+                break;
+            }
+        }
+       
+      const requestOptions = {
+        method: 'DELETE'
+      }
+      fetch('http://3.211.82.27:8800/funfacts/'+this.state.netid+'/'+id, requestOptions)
+      .then(async response => {
+           const data = await response;
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+        
+            this.setState({ oldFunfacts });
+        })
+        .catch(error => {
+            console.error('There was an error!', error);
+        });
+   }
 
 render() {
     var step = (this.state.path != "/settings") ? <StepProgressBar level="75"/> : <div />;
@@ -75,23 +249,25 @@ render() {
                 <div className="content-container">
                   <h3>Create Your Profile</h3>
 		
-		  <h2 className="section-header">Upload Profile Picture</h2>
-		  <ImageUpload callback={this.setProfile}/>
+		  <h2 className="section-header">Step 1: Upload Profile Picture</h2>
+		  <input type="file" className="file-input" onChange={this.setProfile}/>
 		  <hr className="divider" />
 
-		  <h2 className="section-header">Choose a Prompt Question</h2>
-		  <Textbox placeholder="Enter prompt Question" header="Your personal question will be answered by each of your matches." className="form-control"/>
+		  <h2 className="section-header">Step 2: Choose a Prompt Question</h2>
+		  <p className="section-subfont">Each of your matches will answer your question in order to chat with you.</p>
+		  <input placeholder="Enter Prompt Question" defaultValue={this.state.promptQ} className="form-control prompt-control" onChange={this.setPrompt}/>
 
 		  <hr className="divider" />
-
-		  <h2 className="section-header">Upload Fun Facts</h2>
-
-		  <DynamicButton callback={this.setProfile} />
+			
+		  <h2 className="section-header">Step 3: Choose Your Fun Facts</h2>
+		  <p className="section-subfont">Your facts tell your matches more about you: hobbies, travels, memories. Each fun fact is accompanied by a photo. We recommend supplying four.</p>
+		  <DynamicButton oldFunfacts={this.state.oldFunfacts} callback={this.setFunfacts} fileCallback={this.setFileFunfacts} deleteCallback={this.setDeleteFunfact}/>
  
 		  <hr className="divider" />
 
-		  <h2 className="section-header">Upload Your Calendar</h2>
-		  <input type="file" name="file" />	
+		  <h2 className="section-header">Step 4: Upload Your Calendar</h2>
+		  <p className="section-subfont">Upload your calendar's .iso file so we can setup dates for you and your matches.</p>
+		  <input type="file" name="file" className="file-input" onChange={this.setCalendar}/>	
 	
                   <button type="submit" className="btn btn-primary btn-block" onClick={this.changePage}>Submit</button>
 
